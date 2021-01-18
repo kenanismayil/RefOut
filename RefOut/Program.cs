10706d608e7a6dafdc6861b2ae26f39700f6778f
keno1907 <63511659+keno1907@users.noreply.github.com>
2021-01-18 20:32:01 +0300

    RefOut

--- RefOut/Program.cs
+++ RefOut/Program.cs
@@ -6,7 +6,48 @@ namespace RefOut
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ref ve out parametreleri
+
+            int aa = 10;                     //deger atamasi yapilmasa ref parametresi calismaz.Cunki aa'dan deger almasi lazim ki
+                                             //ParamRef'te degisiklik yapabilsin.Bu static void Param(int x) icin de gecerlidir.
+                                             //out parametresi calismasi icin aa'ya baslangic degeri vermek zorunda degiliz.Deger
+                                             //versek de vermesek de bu programda ParamOut 2 olacaktir.
+
+            Console.WriteLine("Param metodu gelmeden once : " + aa);
+
+            Param(aa);
+            Console.WriteLine("Param metodu geldikten sonra : " + aa);
+
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("ParamRef metodu gelmeden once : " + aa);
+            ParamRef(ref aa);                //ParamRef cagrimi şöyle yapilir.
+            Console.WriteLine("ParamRef metodu geldikten sonra : " + aa);
+
+
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("ParamOut metodu gelmeden once : " + aa);
+            ParamOut(out aa);                //ParamRef cagrimi şöyle yapilir.
+            Console.WriteLine("ParamOut metodu geldikten sonra : " + aa);
+
+
+            Console.ReadLine();
         }
+        static void Param(int x)            //aa'nin bellek adresini degil de degerini kopyalar.
+        {
+            x = x * 2;                      //degerini kopyaladigi icin aa'nin icerigini degisemez ve aa once de sonra da ayni kalir.
+        }
+
+        static void ParamRef(ref int x)     //aa'nin bellek adresini x degiskenine ref parametresiyle kopyalar. 
+        {
+            x = x * 2;                      //x dogrudan aa'nin adresine gider ve degeri 20 yapar.
+        }
+
+        static void ParamOut(out int x)
+        {
+            x = 2;                         //Diger parametrelerden farkli olarak out parametresinde metot icinde atama islemi yapilmalidir.Yoksa
+        }                                  //program calismaz,aa degeri x'e gelmez.
+    }
     }
 }
---
+++ RefOutTogether/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RefOutTogether
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int refValue = 7;                              //baslangic degeri atamasi zorunludur.Cunki bu degeri ref parametresine gonderecegiz.
+            int outValue = 17;                                  //baslangic deger atamasi zorunlu degil.
+            int value = 9;
+
+            Console.WriteLine("Before ChangeMethod function is called : " + value);
+            Console.WriteLine("Before ChangeMethod function is called : " + refValue);
+            Console.WriteLine("I don't need to write before ChangeMethod function is called.Because I didn't give value to outValue variable!");
+
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            ChangeMetod(ref refValue, out outValue, value);
+
+            Console.WriteLine("After ChangeMethod function is called : " + value);
+            Console.WriteLine("After ChangeMethod function is called : " + refValue);
+            Console.WriteLine("After ChangeMethod function is called : " + outValue);
+
+            Console.ReadLine();
+        }
+
+        static void ChangeMetod(ref int x, out int y, int z)
+        {
+            z = z + 2;                                     //value degiskeninin orjinalini degil de kopyasini aldigi icin fonksiyon cagirildiktan sonra value'da bir degisiklik olmaz.
+            x = x * 2;                                     //Burada x icin deger atamasi yapmak onemli degil.            
+            y = 17;                                        //Fakat y argumani icin atama yapilmasi zorunludur.
+        }
+    }
+    }
+}
